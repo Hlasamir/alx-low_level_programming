97db925a72ac7655d6b2625f35bc5ee7726e1cca
Hlasamir <hla.aboelmahasen.200@gmail.com>
2023-07-13 18:47:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 write(2,
-"and that piece of art is useful\" - Dora kropar, 2015-10-19\n", 59);
+"and that piece of art is useful\" - Dora Kropar, 2015-10-19\n", 59);
 return (1);
 }
