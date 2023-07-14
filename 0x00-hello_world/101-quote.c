f21f53781f70ab075baeb023d0f521344fa6ecfa
Hlasamir <hla.aboelmahasen.200@gmail.com>
2023-07-13 18:41:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unisted.h>
 /**
 * main - Entry point
 * Return: Always 1 (Success)
@@ -6,6 +7,6 @@
 int main(void)
 {
 fwrite(2,
-"and that piece of art is useful\" - Dora Kropar, 2015-10-19\n", 59);
+"and that piece of art is useful\" - Dora kropar, 2015-10-19\n", 59);
 return (1);
 }
