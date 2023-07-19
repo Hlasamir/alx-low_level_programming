c7aeefaa42eb40c642b8a870f721debbf26a83ae
Hlasamir <hla.aboelmahasen.200@gmail.com>
2023-07-18 17:08:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 int main(void)
 {
 _putchar('_');
@@ -15,6 +15,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
 
