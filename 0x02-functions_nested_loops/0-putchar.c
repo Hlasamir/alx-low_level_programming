f16d2ca72f8d9c87f0ca344bf1103b4408ea5d20
Hlasamir <hla.aboelmahasen.200@gmail.com>
2023-07-19 14:23:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - Print _putchar
  * Return: Always 0
@@ -15,5 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return(0);
 }
