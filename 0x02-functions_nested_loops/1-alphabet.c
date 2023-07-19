15b07c1b18954de55ab7f9a738364e39a5e25fe9
Hlasamir <hla.aboelmahasen.200@gmail.com>
2023-07-19 15:24:36 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet - print all alphabet in lowercase
  */
@@ -8,4 +9,4 @@ void print_alphabet(void)
 	for(letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n')
-	}
+}
